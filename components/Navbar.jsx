aafc1506794ffc8acdf811c5b6c6df1ebb56611c
Yann6666 <yann.cotard01@gmail.com>
2023-08-21 16:21:32 +0200

    3eme corrections

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => (
 
     <div className="absolute w-[50%] inset-0 gradient-01" />
     <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
-      <img src="/search.svg" alt="Loupe de recherche" className='w-[24px] h-[24px] object-contain' />
+      <img src="/search.svg" alt="Loupe de recherche" className="w-[24px] h-[24px] object-contain" />
       <h2 className="font-extrabold text-[24px] leading-[30px] text-white">
         METAVERSUS
       </h2>
@@ -23,7 +23,6 @@ const Navbar = () => (
       <img src="/menu.svg" alt="menu hamburger" className="w-[24px] h-[24px] object-contain" />
 
     </div>
-   
   </motion.nav>
 );
 
